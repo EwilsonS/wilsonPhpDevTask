94cc8f3ce8f85b5713850fe62bc9a14616834699
Evan Wilson <ewilson@aol.com>
2019-02-25 21:33:16 -0500

    include new functionality to mvc frame

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,26 +1,21 @@
 <?php
-    class Pages extends Controller {
-        public function __construct(){
-
-        }
-
-        public function index(){
-            if(isLoggedIn()){
-                redirect('posts');
-            }
-            $data = [
-                'title' => 'Evan\'s Quizzz App',
-                'description' => 'Enter Your Name and Choose a Topic'
-            ];
-
-            $this->view('pages/index', $data);
-        }
-
-        public function about(){
-            $data = [
-                'title' => 'About Us',
-                'description' => 'App to share posts with other users'
-            ];
-            $this->view('pages/about', $data);
-        }
+  class Pages extends Controller {
+    public function __construct(){
     }
+
+    public function index(){
+      $data = [
+        'title' => 'Evan\'s Quizzz App',
+        'description' => 'Enter Your Name and Choose a Topic'
+      ];
+      $this->view('pages/index', $data);
+    }
+
+    public function about(){
+      $data = [
+        'title' => 'About This App',
+        'description' => 'App to share posts with other users'
+      ];
+      $this->view('pages/about', $data);
+    }
+  }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,18 +1,18 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-    <div class="card title-main py-5 rounded-0 text-center">
-        <div class="container">
-            <h1 class="display-3"><?php echo  $data['title']; ?></h1>
-            <p class="text-center">Created by Evan Wilson</p>
-            <p class="lead"><?php echo  $data['description']; ?></p>
-            <input type="text">
-            <br><br>
-            <select name="" id="">
-                <option value="Test 1">Test 1</option>
-                <option value="Test 2">Test 2</option>
-                <option value="Test 3">Test 3</option>
-            </select>
-            <br><br>
-            <input type="submit" value="Take Test" class="btn btn-info">
-        </div>
+  <div class="card title-main py-5 rounded-0 text-center">
+    <div class="container">
+      <h1 class="display-3 text-light"><?php echo  $data['title']; ?></h1>
+      <p class="text-center text-light">Created by Evan Wilson</p>
+      <p class="lead text-light"><?php echo  $data['description']; ?></p>
+      <input type="text">
+      <br><br>
+      <select name="" id="">
+          <option value="Test 1">Test 1</option>
+          <option value="Test 2">Test 2</option>
+          <option value="Test 3">Test 3</option>
+      </select> 
+      <br><br>
+      <input type="submit" value="Take Test" class="btn btn-info">
     </div>
+  </div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
