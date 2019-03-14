40784925779f9399b66931ab7655c6e5903bc8b1
Evan Wilson <ewilson@aol.com>
2019-03-13 23:05:09 -0400

    update

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,13 @@
 <?php
     //DB Params
+
+    //Deployed
     // define('DB_HOST', 'y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
     // define('DB_USER', 'y9g8hwat6pa22eed');
     // define('DB_PASS', 'wfd3wdljn4qtoy2q');
     // define('DB_NAME', 'x8iaakv6u5hlrr1t');
+
+    //Dev
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', 'kalamazoo123');
